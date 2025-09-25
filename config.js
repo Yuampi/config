55c423de29e4565b9ca4a8fe1c90768a2240c476
Yuampi <110417281+Yuampi@users.noreply.github.com>
2025-09-25 00:19:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,70 +3,56 @@
  * For more information on how you can configure this file
  * see https://docs.magicmirror.builders/configuration/introduction.html
  * and https://docs.magicmirror.builders/modules/configuration.html
- *
- * You can use environment variables using a `config.js.template` file instead of `config.js`
- * which will be converted to `config.js` while starting. For more information
- * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
  */
-let config = {
-	address: "localhost",	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
-	port: 8080,
-	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
-									// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
-									// or add a specific IPv4 of 192.168.1.5 :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
-	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
-	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
-	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+let config = {
+	address: "localhost",
+	port: 8080,
+	basePath: "/",
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+
+	useHttps: false,
+	httpsPrivateKey: "",
+	httpsCertificate: "",
 
 	language: "en",
-	locale: "en-US",   // this variable is provided as a consistent location
-			   // it is currently only used by 3rd party modules. no MagicMirror code uses this value
-			   // as we have no usage, we  have no constraints on what this field holds
-			   // see https://en.wikipedia.org/wiki/Locale_(computer_software) for the possibilities
-
-	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	locale: "en-US",
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"],
 	timeFormat: 24,
 	units: "metric",
 
 	modules: [
-{
-    module: 'MMM-SleepWake',
-    config:{
-       delay:  15,               // default
-       mode:  "HIDE",    
-	}
-}{
-    module: 'camera',
-    position: 'top_center',
-    config: selfieInterval: 3,  // Time interval in seconds before the photo will be taken.
-		emailConfig: {
-			service: 'Hotmail', // Email provider to use to send email with a photo.
-			auth: {
-				user: '<name@email.com>', // Your email account
-				pass: '<password>'        // Your password for email account
-			}
-		}
-  {
-    module: 'MMM-WiFiPassword',
-    position: "top_left",
-      config: {
-        //See 'Configuration options' for more information.
-        network: "Fibertel WiFi321 2.4Ghz", 
-        password: "21316365",
-      }
-  },
-
 		{
-			module: "alert",
+			module: 'MMM-SleepWake',
+			config: {
+				delay: 15,
+				mode: "HIDE"
+			}
+		},
+		{
+			module: 'camera',
+			position: 'top_center',
+			config: {
+				selfieInterval: 3,
+				emailConfig: {
+					service: 'Hotmail',
+					auth: {
+						user: '<name@email.com>',
+						pass: '<password>'
+					}
+				}
+			}
+		},
+		{
+			module: 'MMM-WiFiPassword',
+			position: "top_left",
+			config: {
+				network: "Fibertel WiFi321 2.4Ghz",
+				password: "21316365"
+			}
+		},
+		{
+			module: "alert"
 		},
 		{
 			module: "updatenotification",
@@ -130,7 +116,7 @@ let config = {
 				broadcastNewsFeeds: true,
 				broadcastNewsUpdates: true
 			}
-		},
+		}
 	]
 };
 
